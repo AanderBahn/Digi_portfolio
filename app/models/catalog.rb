b0cb4726e3a3409f86f00a04c8c7260bac25b848
Alexander Bahn <AanderBahn@gmail.com>
2015-11-03 18:00:39 -0600

    NavBar Bug Fix

--- app/models/catalog.rb
+++ app/models/catalog.rb
@@ -1,2 +1,16 @@
 class Catalog < ActiveRecord::Base
+
+  def catHist
+    if category = "Historical"
+    end
+  end
+
+  def catThea
+  end
+
+  def catMod
+  end
+
+
+  #has_attached_file :???, :styles => { :medium => "300x300#", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
 end
